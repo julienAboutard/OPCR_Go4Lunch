c4649feee83cb7d7b16bb139f260fcec335599a5
Julien <julien.aboutard@gmail.com>
2024-10-16 13:50:10 +0200

    End of Design, need to work on some details

---
+++ app/src/main/java/com/juliena/go4lunch/ListRestaurantViewModel.java
@@ -0,0 +1,7 @@
+package com.juliena.go4lunch;
+
+import androidx.lifecycle.ViewModel;
+
+public class ListRestaurantViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/juliena/go4lunch/ListWorkmateFragment.java
@@ -0,0 +1,36 @@
+package com.juliena.go4lunch;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListWorkmateFragment extends Fragment {
+
+    private ListWorkmateViewModel mViewModel;
+
+    public static ListWorkmateFragment newInstance() {
+        return new ListWorkmateFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list_workmate, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ListWorkmateViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
